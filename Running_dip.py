ef1b8f0a813bf515dcedfd2ab19d753d4e960bc1
Andreas Kattamis <kattamis.a@gmail.com>
2018-12-01 00:22:19 +0000

    new images

--- Running_dip.py
+++ Running_dip.py
@@ -9,13 +9,19 @@ from functions.classification import *
 import torch
 
 
-adv, orig, pert = adversarial_examples("data/goldfish.jpg",method = "LLCI",eps=100, num_iter=125,show=False)
+#adv, orig, pert = adversarial_examples("data/goldfish.jpg",method = "LLCI",eps=100, num_iter=125,show=False)
 
-for i in range(3):
-	std1 = 2**(i+3)
-        std = std1/64
-	out = dip(adv,num_iter=10000,save=True,PLOT=False,reg_noise_std = std1,save_path='results/Adv_DIP/Std_complex_{}-64'.format(std1),arch='complex',input_depth=32)
-	
-for i in range(6):
-	ids = 2**(i+1)
-	out = dip(adv,num_iter=10000,save=True,PLOT=False,save_path='results/Adv_DIP/ID_complex_{}'.format(ids),arch='complex',input_depth=ids)
+#for i in range(3):
+#	std1 = 2**(i+3)
+#        std = std1/64
+#	out = dip(adv,num_iter=10000,save=True,PLOT=False,reg_noise_std = #std1,save_path='results/Adv_DIP/Std_complex_{}-64'.format(std1),arch='complex',input_depth=32)
+#	
+#for i in range(6):
+#	ids = 2**(i+1)
+#	out = dip(adv,num_iter=10000,save=True,PLOT=False,save_path='results/Adv_DIP/ID_complex_{}'.format(ids),arch='complex',input_depth=ids)
+
+images = ['panda.jpg','peacock.jpg','F16_GT.png','monkey.jpg','zebra_GT.png']
+for i in images:
+    adv, orig, pert = adversarial_examples("data/{}".format(i),method = "LLCI",eps=100, num_iter=125,show=False)
+    out = dip(adv,num_iter=10001,save=True,PLOT=False,save_path='results/Adv_DIP/{}'.format(i),arch='complex')
+    
