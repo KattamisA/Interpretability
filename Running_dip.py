6377a9e9ad8aef0fa958f000c51873067e7b320c
Andreas Kattamis <akattamis@al747pc>
2018-12-01 07:43:56 +0000

    Run some tests, on input noise and on input depth, for the complex architecture

--- Running_dip.py
+++ Running_dip.py
@@ -11,8 +11,8 @@ import torch
 
 adv, orig, pert = adversarial_examples("data/goldfish.jpg",method = "LLCI",eps=100, num_iter=125,show=False)
 
-for i in range(4):
-	std1 = 2**(i+2)
+for i in range(3):
+	std1 = 2**(i+3)
         std = std1/64
 	out = dip(adv,num_iter=10000,save=True,PLOT=False,reg_noise_std = std1,save_path='results/Adv_DIP/Std_complex_{}-64'.format(std1),arch='complex',input_depth=32)
 	
