bfd64efdc42447851d055016149604c2e70cd38a
Andreas Kattamis <kattamis.a@gmail.com>
2018-12-11 13:45:05 +0000

    Testing on the std

--- running_dip.py
+++ running_dip.py
@@ -10,7 +10,7 @@ num_iter = 10001
 for i in range(7):
     std1 = 2**(i)
     std = std1/64.0
-    save_path = 'results/Adv_DIP/Std_complex_{}-64'.format(std1)
+    save_path = 'results/Adv_DIP/goldfish/Std_complex_{}-64'.format(std1)
     out = dip(adv, num_iter=num_iter, save=True, plot=False, reg_noise_std = std1,
               save_path = save_path, arch='complex', input_depth=32)
     generate_result_files(save_path, adv, orig, num_iter)
