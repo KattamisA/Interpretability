2da16bdce0ce216f0d907b61d64bf2f23b8ae27d
Andreas Kattamis <kattamis.a@gmail.com>
2018-12-14 14:15:01 +0000

    .m files

--- results/Adv_DIP/Goldfish/Adv_DIP_plotting.m
+++ results/Adv_DIP/Goldfish/Adv_DIP_plotting.m
@@ -4,9 +4,9 @@ close all
 
 %% Create containers
 Confidence = zeros(100,6);
-Std_conf = zeros(100,6);
+Std_conf = zeros(101,5);
 simple_FGSM_conf = zeros(101,3);
-f = 1;
+f = 5;
 %% Load variables
 load 'LLCI_eps100/LLCI_eps100_full.txt'
 load 'LLCI_eps25/LLCI_eps25_full.txt'
@@ -31,55 +31,55 @@ simple_FGSM_conf(:,3) = smooth(Simple_FGSM_100_full(:,1),f);
 load 'Complex_LLCI_eps100/Complex_LLCI_eps100_full.txt'
 
 
-load 'Std_complex_1-64/Confidences.txt'
-Std_conf(:,1) = smooth(Confidences(:,1),f);
+% load 'Std_complex_1-64/Confidences.txt'
+% Std_conf(:,1) = smooth(Confidences(:,1),f);
 load 'Std_complex_2-64/Confidences.txt'
-Std_conf(:,2) = smooth(Confidences(:,1),f);
+Std_conf(:,1) = smooth(Confidences(:,1),f);
 load 'Std_complex_4-64/Confidences.txt'
-Std_conf(:,3) = smooth(Confidences(:,1),f);
+Std_conf(:,2) = smooth(Confidences(:,1),f);
 load 'Std_complex_8-64/Confidences.txt'
-Std_conf(:,4) = smooth(Confidences(:,1),f);
+Std_conf(:,3) = smooth(Confidences(:,1),f);
 load 'Std_complex_16-64/Confidences.txt'
-Std_conf(:,5) = smooth(Confidences(:,1),f);
+Std_conf(:,4) = smooth(Confidences(:,1),f);
 load 'Std_complex_32-64/Confidences.txt'
-Std_conf(:,6) = smooth(Confidences(:,1),f);
+Std_conf(:,5) = smooth(Confidences(:,1),f);
+% 
+% load 'ID_complex_2/Confidences.txt'
+% Confidence(:,1) = smooth(Confidences(:,1),f);
+% load 'ID_complex_4/Confidences.txt'
+% Confidence(:,2) = smooth(Confidences(:,1),f);
+% load 'ID_complex_8/Confidences.txt'
+% Confidence(:,3) = smooth(Confidences(:,1),f);
+% load 'ID_complex_16/Confidences.txt'
+% Confidence(:,4) = smooth(Confidences(:,1),f);
+% load 'ID_complex_32/Confidences.txt'
+% Confidence(:,5) = smooth(Confidences(:,1),f);
+% load 'ID_complex_64/Confidences.txt'
+% Confidence(:,6) = smooth(Confidences(:,1),f);
+% t=0:100:9900;
+% plot(t,Confidence);
+% 
+% legend('Input depth = 2','Input depth = 4','Input depth = 8','Input depth = 16','Input depth = 32','Input depth = 64');
+% xlabel('DIP iterations');
+% ylabel('Confidence of true class');
+% 
+% figure
+% plot(t,reshape(smooth(Confidences(:,[1,2]),f),100,2))
+% xlabel('DIP iterations');
+% ylabel('Class Confidence');
 
-load 'ID_complex_2/Confidences.txt'
-Confidence(:,1) = smooth(Confidences(:,1),f);
-load 'ID_complex_4/Confidences.txt'
-Confidence(:,2) = smooth(Confidences(:,1),f);
-load 'ID_complex_8/Confidences.txt'
-Confidence(:,3) = smooth(Confidences(:,1),f);
-load 'ID_complex_16/Confidences.txt'
-Confidence(:,4) = smooth(Confidences(:,1),f);
-load 'ID_complex_32/Confidences.txt'
-Confidence(:,5) = smooth(Confidences(:,1),f);
-load 'ID_complex_64/Confidences.txt'
-Confidence(:,6) = smooth(Confidences(:,1),f);
-t=0:100:9900;
-plot(t,Confidence);
-
-legend('Input depth = 2','Input depth = 4','Input depth = 8','Input depth = 16','Input depth = 32','Input depth = 64');
+figure
+plot(0:100:10000,Std_conf)
 xlabel('DIP iterations');
 ylabel('Confidence of true class');
+legend('show')
+% figure
+% plot(0:100:10000,simple_FGSM_conf)
+% xlabel('DIP iterations');
+% ylabel('Confidence of true class');
 
-figure
-plot(t,reshape(smooth(Confidences(:,[1,2]),f),100,2))
-xlabel('DIP iterations');
-ylabel('Class Confidence');
-
-figure
-plot(t,Std_conf)
-xlabel('DIP iterations');
-ylabel('Confidence of true class');
-
-figure
-plot(0:100:10000,simple_FGSM_conf)
-xlabel('DIP iterations');
-ylabel('Confidence of true class');
-
-figure
-plot(0:100:9100,Complex_LLCI_eps100_full(:,[1,2]))
+% figure
+% plot(0:100:9100,Complex_LLCI_eps100_full(:,[1,2]))
 %% Plotting all the LLCI true classes confidences
 
 % True_class_confidence = [LLCI_eps5_full(:,1), LLCI_eps25_full(:,1), LLCI_eps100_full(:,1)];
