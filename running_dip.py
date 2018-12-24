12801db34d095ed08e68036093eff125e2fc2de7
Andreas Kattamis <kattamis.a@gmail.com>
2018-12-24 19:59:32 +0200

    std results for multiple images

--- running_dip.py
+++ running_dip.py
@@ -34,7 +34,7 @@ num_iter = 10001
 #    generate_result_files(save_path, adv, orig, num_iter)
 
 ### Observing multiple images #
-images = ['panda.jpg','peacock.jpg','F16_GT.png','monkey.jpg','zebra_GT.png','goldfish.jpg']#'whale.jpg', 'dolphin.jpg', 'spider.jpg', 'labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg', 'car_wheel.jpg','fountain.jpg', 'football_helmet.jpg','hourglass.jpg', 'refrigirator.jpg']
+images = ['F16_GT.png','monkey.jpg','zebra_GT.png','goldfish.jpg']#'whale.jpg', 'dolphin.jpg', 'spider.jpg', 'labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg', 'car_wheel.jpg','fountain.jpg', 'football_helmet.jpg','hourglass.jpg', 'refrigirator.jpg']
 #images = ['goldfish.jpg','panda.jpg', 'peacock.jpg']
 
 std1 = [1 ,2 ,4, 8, 16, 32, 1]
