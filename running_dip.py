46f6e56c8498059cfd9b8809f2b6d5145235c813
Andreas Kattamis <kattamis.a@gmail.com>
2018-12-30 14:19:35 +0200

    multiple images for all adv. methods

--- running_dip.py
+++ running_dip.py
@@ -34,7 +34,7 @@ num_iter = 10001
 #    generate_result_files(save_path, adv, orig, num_iter)
 
 ### Observing multiple images #
-images = [ 'peacock.jpg','labrador.jpg']#, 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg', 'car_wheel.jpg','fountain.jpg', 'football_helmet.jpg','hourglass.jpg', 'refrigirator.jpg','knife.jpg','rope.jpeg']
+images = [ 'panda.jpg','peacock.jpg','F16_GT.png','monkey.jpg','zebra_GT.png','goldfish.jpg','whale.jpg', 'dolphin.jpg', 'spider.jpg','labrador.jpg']#, 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg', 'car_wheel.jpg','fountain.jpg', 'football_helmet.jpg','hourglass.jpg', 'refrigirator.jpg','knife.jpg','rope.jpeg']
 #'panda.jpg','peacock.jpg',
 #'F16_GT.png','monkey.jpg','zebra_GT.png','goldfish.jpg','whale.jpg', 'dolphin.jpg', 'spider.jpg',
 eps = [25, 100]
@@ -55,23 +55,23 @@ eps = [25, 100]
         #save_path='results/Adv_DIP/Std_investigation/EntropySGD_LR10/{}'.format(i.split('.')[0])
         #out = dip(adv, num_iter=num_iter, save=True, plot=False, save_path = save_path, arch='complex', OPTIMIZER = "EntropySGD", LR = 10)
         #generate_result_files(save_path, adv, orig, num_iter)
-images = ['labrador.jpg']#,'peacock.jpg','labrador.jpg']
-eps = [100]
-for i in images:
-    for j in range(1):
-        adv, orig, pert = adversarial_examples("data/{}".format(i), method = "BI", eps=eps[j], show=False)
-        print("#############\n\n Epsilon = {}  -  Working on image: {}".format(eps[j],i.split('.')[0]))       
-        name = '{}'.format(i.split('.')[0])
-        save_path='results/Adv_DIP/All_adv_methods/BI_eps{}'.format(eps[j])
-        out = dip(adv, num_iter=num_iter, save=True, plot=False, save_path = save_path, arch='complex')
-        generate_result_files(save_path, adv, orig, num_iter, name)   
-    
-#eps = [1, 5, 25, 100]
+#images = ['labrador.jpg']#,'peacock.jpg','labrador.jpg']
+#eps = [100]
 #for i in images:
-#    for j in range(2):
-#        adv, orig, pert = adversarial_examples("data/{}".format(i), method = "FGSM", eps=eps[j], show=False)
+#    for j in range(4):
+#        adv, orig, pert = adversarial_examples("data/{}".format(i), method = "BI", eps=eps[j], show=False)
 #        print("#############\n\n Epsilon = {}  -  Working on image: {}".format(eps[j],i.split('.')[0]))       
 #        name = '{}'.format(i.split('.')[0])
-#        save_path='results/Adv_DIP/All_adv_methods/FGSM_eps{}'.format(eps[j])
+#        save_path='results/Adv_DIP/All_adv_methods/BI_eps{}'.format(eps[j])
 #        out = dip(adv, num_iter=num_iter, save=True, plot=False, save_path = save_path, arch='complex')
-#        generate_result_files(save_path, adv, orig, num_iter, name)
+#        generate_result_files(save_path, adv, orig, num_iter, name)   
+    
+eps = [1, 5, 25, 100]
+for i in images:
+    for j in range(4):
+        adv, orig, pert = adversarial_examples("data/{}".format(i), method = "FGSM", eps=eps[j], show=False)
+        print("#############\n\n Epsilon = {}  -  Working on image: {}".format(eps[j],i.split('.')[0]))       
+        name = '{}'.format(i.split('.')[0])
+        save_path='results/Adv_DIP/All_adv_methods/FGSM_eps{}'.format(eps[j])
+        out = dip(adv, num_iter=num_iter, save=True, plot=False, save_path = save_path, arch='complex')
+        generate_result_files(save_path, adv, orig, num_iter, name)
