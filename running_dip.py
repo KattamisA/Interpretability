9a164cf3beaa3cd0b970a6988d993e6d2bfe69c9
Andreas Kattamis <kattamis.a@gmail.com>
2019-01-05 11:17:25 +0200

    tests on architecture

--- running_dip.py
+++ running_dip.py
@@ -34,8 +34,8 @@ num_iter = 10001
 #    generate_result_files(save_path, adv, orig, num_iter)
 
 ### Observing multiple images #
-images = ['panda.jpg','peacock.jpg','F16_GT.png','monkey.jpg','zebra_GT.png','goldfish.jpg','whale.jpg', 'dolphin.jpg', 'spider.jpg','labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg', 'car_wheel.jpg','fountain.jpg', 'football_helmet.jpg','hourglass.jpg', 'refrigirator.jpg','knife.jpg','rope.jpeg']
-
+images = ['goldfish.jpg','whale.jpg', 'dolphin.jpg', 'spider.jpg','labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg', 'car_wheel.jpg','fountain.jpg', 'football_helmet.jpg','hourglass.jpg', 'refrigirator.jpg','knife.jpg','rope.jpeg']
+#'panda.jpg','peacock.jpg','F16_GT.png','monkey.jpg','zebra_GT.png',
 for i in images:
     adv, orig, pert = adversarial_examples("data/{}".format(i), method = "LLCI", eps = 100, show=False)
     print("#############\n\nWorking on image: {}".format(i.split('.')[0]))           
