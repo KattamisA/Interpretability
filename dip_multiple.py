469414b0fa9f87159113c0e2e41b2a8752c43c35
Andreas Kattamis <kattamis.a@gmail.com>
2019-01-22 23:27:07 +0000

    skip connection tests

--- dip_multiple.py
+++ dip_multiple.py
@@ -11,6 +11,25 @@ image_dataset = ['panda.jpg','peacock.jpg','F16_GT.png','monkey.jpg','zebra_GT.p
 for i in range(0,len(image_dataset)):
     image_path = image_dataset[i]
     image_name = '{}'.format(image_path.split('.')[0])
+    save_path_common = 'results/Adv_DIP/Skip_connections/{}'
+                
+    print("#############\n\nWorking on image: {}".format(image_name))           
+    adv, orig, pert = adversarial_examples("data/{}".format(image_path), method = "LLCI", eps = 100, show=False)
+    
+    for j in range(1,6):  
+        print("####\n\nTest {}".format(j))           
+
+        save_path=save_path_common.format('Adam/test{}'.format(j))
+        out = dip(adv,'skip{}'.format(j), 0.01, num_iter, save=True, plot=False, save_path = save_path)
+        generate_result_files(save_path, adv, orig, num_iter, image_name)
+
+        save_path=save_path_common.format('EntropySGD_std64/test{}'.format(j))
+        out = dip(adv,'skip{}'.format(j), 10, num_iter, 1/64., save=True, plot=False, save_path = save_path, OPTIMIZER = "EntropySGD")
+        generate_result_files(save_path, adv, orig, num_iter, image_name)
+        
+for i in range(13,len(image_dataset)):
+    image_path = image_dataset[i]
+    image_name = '{}'.format(image_path.split('.')[0])
     save_path_common = 'results/Adv_DIP/Architecture_tests/{}'
                 
     print("#############\n\nWorking on image: {}".format(image_name))           
@@ -26,3 +45,5 @@ for i in range(0,len(image_dataset)):
         save_path=save_path_common.format('EntropySGD_std64/test{}'.format(j))
         out = dip(adv,'test{}'.format(j), 10, num_iter, 1/64., save=True, plot=False, save_path = save_path, OPTIMIZER = "EntropySGD")
         generate_result_files(save_path, adv, orig, num_iter, image_name)
+        
+       
