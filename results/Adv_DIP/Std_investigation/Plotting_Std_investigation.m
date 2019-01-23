cb2d372a3f93f4d1465ec57ace8a7aa52b64d316
Andreas Kattamis <kattamis.a@gmail.com>
2019-01-23 21:19:19 +0000

    skip connection tests

--- results/Adv_DIP/Std_investigation/Plotting_Std_investigation.m
+++ results/Adv_DIP/Std_investigation/Plotting_Std_investigation.m
@@ -11,23 +11,38 @@ common = 'EntropySGD/%s_%d_256_Normalised.txt';
 std1 = [1, 2, 4, 8];
 Averaged= zeros(101,size(std1,2));
 Std = zeros(101,size(std1,2));
-
+Peak_iterations = zeros(size(std1,2),size(img_names,2));
+Peak_amplitudes = zeros(size(std1,2),size(img_names,2));
+Mean_peak_amplitudes = zeros(size(std1,2),size(img_names,2));
+Mean_iteration = zeros(size(std1,2),1);
 %% Filling in the matrices
 for j=1:size(std1,2)
     for i=1:size(img_names,2)
         path = sprintf(common,img_names(i),std1(j));
         s = load(path);
-        Confidence(:,i) = s(:,1);       
+        Confidence(:,i) = s(:,1);
+        [Peak_amplitudes(j,i),Peak_iterations(j,i)]  = max(s(1:51,1));
     end
     Averaged(:,j) = smooth(mean(Confidence,2));
     Std(:,j) = std(Confidence,0,2);
+%     Mean_iteration(j) = round(mean(Peak_iterations(j,:)));
+    [M,Mean_iteration(j)] = max(Averaged(:,j));
+    Mean_peak_amplitudes(j,:) = Confidence(Mean_iteration(j),:);    
+    peak_mean_amps(j) = mean(Mean_peak_amplitudes(j,:));
+    peak_std_amps(j) = std(Mean_peak_amplitudes(j,:));
+    
+    hold on
+    plot([peak_mean_amps(j)-1*peak_std_amps(j),peak_mean_amps(j) + 1*peak_std_amps(j)],[j,j],'LineWidth',20)
+    plot(Mean_peak_amplitudes(j,:),j,'xk','MarkerSize',8)
 end
+ylim([0,size(std1,2)+1])
+grid on
 
 
 
 %% Plotting
-plot(t,Std,'LineWidth',1)
-legend('show')
+% plot(t,Std,'LineWidth',1)
+% legend('show')
 % figure
 % hold on
 % plot(0:100:5000,Averaged(1:51,3),'b','LineWidth',1.5)
