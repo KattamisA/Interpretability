85d825d4904c162c10a236dd55c6391f6c142ec8
Andreas Kattamis <kattamis.a@gmail.com>
2019-01-30 15:49:57 +0000

    smoothgrad addition

--- functions/generate_saliency_maps.py
+++ functions/generate_saliency_maps.py
@@ -43,6 +43,7 @@ def generate_saliency_maps(path, img_path, model_type='resnet18', cuda=False, to
     gradients, label_index = calculate_outputs_and_gradients([img], model, None, cuda)
     gradients = np.transpose(gradients[0], (1, 2, 0))
     smoothedgrad_gradients = get_smoothed_gradients([img], model, label_index, calculate_outputs_and_gradients, cuda=True)
+    print(np.shape(smoothedgrad_gradients))
     img_gradient_overlay = visualize(smoothedgrad_gradients, img, clip_above_percentile=top_percentile, clip_below_percentile=bottom_percentile, overlay=overlay, mask_mode=mask_mode)
     img_gradient = visualize(smoothedgrad_gradients, img, clip_above_percentile=top_percentile, clip_below_percentile=bottom_percentile, overlay=False)
     print(np.shape(img_gradient))
