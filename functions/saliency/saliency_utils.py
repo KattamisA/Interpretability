367498f489f96173c443abe124632f8c2c06f12b
Andreas Kattamis <kattamis.a@gmail.com>
2019-01-30 15:27:58 +0000

    smoothgrad addition

--- functions/saliency/saliency_utils.py
+++ functions/saliency/saliency_utils.py
@@ -64,7 +64,7 @@ def get_smoothed_gradients(x_value, model, target_label_idx, predict_and_gradien
         noise = np.random.normal(0, stdev, np.shape(x_value))
         x_plus_noise = x_value + noise
         grad, _ = predict_and_gradients([x_plus_noise], model, target_label_idx, cuda)
-        grad = np.transpose(..., (1, 2, 0))
+        grad = np.transpose(:, :, :, (1, 2, 0))
         if magnitude:
             total_gradients += (grad * grad)
         else:
