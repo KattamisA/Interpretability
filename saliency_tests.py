f639c10a940467a07f76f9358be0c3e2c54cdb02
Andreas Kattamis <kattamis.a@gmail.com>
2019-01-30 12:42:09 +0000

    rearrangments and optimization

--- saliency_tests.py
+++ saliency_tests.py
@@ -1,4 +1,4 @@
-from functions.saliency import *
+from functions.saliency.saliency import *
 
 image_dataset = ['panda.jpg']#, 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_GT.png', 'goldfish.jpg', 'whale.jpg',
                  #'dolphin.jpg', 'spider.jpg', 'labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg',
