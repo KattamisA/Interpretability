b888c37aa3b59f96e4161043cd1ccbfc41898879
Andreas Kattamis <kattamis.a@gmail.com>
2019-01-30 00:00:17 +0000

    testing saliency

--- Saliency_tests.py
+++ Saliency_tests.py
@@ -10,7 +10,7 @@ image_dataset = ['panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_
 
 for i in range(len(image_dataset)):
     image = image_dataset[i]
-    print('\n\n ### Working on image: ' + image.split('.')[0])
+    print('\n### Working on image: ' + image.split('.')[0])
     generate_saliency_maps('results/Saliency', image, cuda=True, top_percentile=99, bottom_percentile=1, mask_mode=True)
-
+    print('\n')
 
