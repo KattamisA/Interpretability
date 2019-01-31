8d57c20e6d2a4431b8cc5dbc5597d8af6749fda1
Andreas Kattamis <kattamis.a@gmail.com>
2019-01-31 17:43:53 +0000

    test

--- functions/saliency/integrated_gradients.py
+++ functions/saliency/integrated_gradients.py
@@ -15,12 +15,13 @@ def integrated_gradients(inputs, model, target_label_idx, predict_and_gradients,
 
     if smoothgrad is False:
         grads, _ = predict_and_gradients(scaled_inputs, model, target_label_idx, cuda)
+        avg_grads = np.average(grads[:-1], axis=0)
         grads = np.transpose(grads, (1, 2, 0))
     else:
         grads = get_smoothed_gradients(scaled_inputs, model, target_label_idx, predict_and_gradients, cuda=cuda,
                                        magnitude=magnitude)
-
-    avg_grads = np.average(grads[:-1], axis=0)
+        avg_grads = np.average(grads[:-1], axis=0)
+        
     integrated_grad = (inputs - baseline) * avg_grads
     return integrated_grad
 
