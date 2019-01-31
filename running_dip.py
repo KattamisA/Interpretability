f0dcd19e5ac1f21d1920401b78c335e720237883
Andreas Kattamis <kattamis.a@gmail.com>
2019-01-31 20:34:11 +0000

    ret

--- running_dip.py
+++ running_dip.py
@@ -39,7 +39,7 @@ for i in images:
     print("#############\n\nWorking on image: {}".format(i.split('.')[0]))
     name = '{}'.format(i.split('.')[0])
     
-    save_path='results/Adv_DIP/Multiple_images
+    save_path='results/Adv_DIP/Multiple_images'
     _ = dip(adv, num_iter=num_iter, save=True, plot=False, save_path = save_path, arch='test')
     #generate_result_files(save_path, adv, orig, num_iter, name)
     
