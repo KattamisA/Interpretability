eaec8c429133d09f7264d29a6a5af834867508f7
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-06 22:43:52 +0000

    grad tests

---
+++ results/Adv_DIP/Multiple_images/PLotting_gradient_convergence.m
@@ -0,0 +1,84 @@
+% clear all
+% close all
+
+% %% Load and plot confidences
+% t=0:100:10000;
+img_names = ["panda", "peacock", "F16_GT", "monkey",'zebra_GT','goldfish'];%,'whale','dolphin','spider','labrador','snake','flamingo_animal','canoe','car_wheel','fountain','football_helmet','hourglass','refrigirator','knife','rope'];
+
+Confidence = zeros(101,size(img_names,2));
+Grads = zeros(101,size(img_names,2));
+hold on
+
+Peak_iterations = zeros(1,size(img_names,2));
+Peak_amplitudes = zeros(1,size(img_names,2));
+Mean_peak_amplitudes = zeros(1,size(img_names,2));
+
+%% Calculating normalised
+common = 'Gradients/Adam/%s_stats.txt';
+for i=1:size(img_names,2)
+    path = sprintf(common,img_names(i));
+    s = load(path);
+    Grads(:,i) = abs(s(:,4));
+end
+Average = mean(Grads,2);
+
+plot(100:100:size(Grads,1)*100, abs(Grads), 'LineWidth',2)
+grid on
+xlabel('DIP iterations')
+ylabel("Absolute mean gradient")
+figure
+hold on
+plot(100:100:size(Grads,1)*100, Average/Average(1), 'LineWidth',2)
+grid on
+xlabel('DIP iterations')
+% ylabel("Averaged absolute mean gradient")
+% figure
+x = diff(Average/100);
+plot(100:100:(size(Grads,1)-1)*100, x./x(1), 'LineWidth',2)
+grid on
+xlabel('DIP iterations')
+% ylabel("First Derivative of mean gradient")
+% figure
+y = diff(x)/100;
+plot(100:100:(size(Grads,1)-2)*100, y./y(1), 'LineWidth',2)
+grid on
+xlabel('DIP iterations')
+% ylabel("Second Derivative of mean gradient")
+
+ylabel('Normalized value')
+legend('Actual value','First derivative','Second derivative')
+%% Calculating Unnormalized
+% common = 'EntropySGD_LR10/%s/Confidences.txt';
+% for i=1:size(img_names,2)
+%     path = sprintf(common,img_names(i));
+%     q = load(path);
+%     Confidence(:,i) = q(:,1);
+%     [Peaks(1,i),Peaks(2,i)]  = max(q(1:51,1));
+% end
+
+%% Plotting
+% Average = mean(Normalise,2);
+% Std = std(Normalise,0,2);
+% 
+% % % plot(t,mean(Confidence,2))
+% hold on
+% plot(t,Average,'r','LineWidth',1.5)
+% plot(t,[Average+Std, Average-Std],'--r','LineWidth',0.2,'HandleVisibility','off')
+% % legend('Adam optimizer - average')
+% xlabel('DIP iterations')
+% ylabel('Averaged Class Confidence')
+% % xlim([0 5000])
+% % plot(t,Std,'LineWidth',1)
+% grid on
+% legend('Adam','EntropySGD')
+%% Plotting all the lines 
+% figure
+% plot(t,Normalise)
+% xlabel('DIP iterations')
+% ylabel('Class Confidence')
+%
+% figure
+% plot(t,Confidence)
+% xlabel('DIP iterations')
+% ylabel('Class Confidence')
+% legend('Panda','Peacock','F16 GT','Monkey','Zebra GT','Goldfish')
--- saliency_tests.py
+++ saliency_tests.py
@@ -1,6 +1,8 @@
 import cv2
 from functions.generate_saliency_maps import generate_saliency_maps
 from functions.classification import classification
+from functions.adversarial import *
+import matplotlib.pyplot as plt
 
 #image_dataset = ['labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg',
 #                 'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
@@ -9,15 +11,25 @@ from functions.classification import classification
 #'panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_GT.png', 'goldfish.jpg', 'whale.jpg','dolphin.jpg', 'spider.jpg',
 #image_dataset = ['it_{}.png'.format(100*i) for i in range(0, 101)]
 #image_dataset = ['it_0.png', 'it_100.png']
-image_dataset = ['it_{}.png'.format(100 * i) for i in range(60, 101)]
+#image_dataset = ['it_{}.png'.format(100 * i) for i in range(60, 101)]
+image_dataset = [1]
 for i in range(len(image_dataset)):
     image = image_dataset[i]
-    orig = cv2.imread('data/knife.jpg')[..., ::-1]
+    adv, orig, pert = adversarial_examples("data/zebra_GT.jpg", method = "LLCI",eps=100, show=False)
+    #orig = cv2.imread('data/knife.jpg')[..., ::-1]
+    plt.imsave("data/Adversarial_knife.png".format(save_path, iter_value),
+               #       np.clip(torch_to_np(glparam.out_avg), 0, 1).transpose(1, 2, 0), format="png")
 
-    _, ranks =classification(orig, model_name='resnet18', sort=True, show=False)
+               _, ranks =classification(orig, model_name='resnet18', sort=True, show=False)
     class_index = ranks[0, 0]
-    print('###### Working on image: ' + image.split('.')[0])
-    generate_saliency_maps('results/Adv_DIP/Multiple_images/knife', image, model_type='resnet18', cuda=True,
-                           top_percentile=99, bottom_percentile=0, mask_mode=True, target_label_index=class_index)
+    #print('###### Working on image: ' + image.split('.')[0])
+    #generate_saliency_maps('results/Adv_DIP/Multiple_images/knife', image, model_type='resnet18', cuda=True,
+    #                       top_percentile=99, bottom_percentile=0, mask_mode=True, target_label_index=class_index)
+
+    generate_saliency_maps('results/Adv_DIP/Multiple_images/zebra_GT', "Adversarial_knife.png", model_type='resnet18', cuda=True,
+                          top_percentile=99, bottom_percentile=0, mask_mode=True, target_label_index=class_index)
+
+
+
     print('\n')
 
