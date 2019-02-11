c3c17b635abb5c3d52923b5a0cf5037831e0f590
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-11 15:26:42 +0000

    test

--- functions/dip.py
+++ functions/dip.py
@@ -340,8 +340,8 @@ def dip(img_np, arch = 'default', LR = 0.01, num_iter = 1000, reg_noise_std = 1.
                 f = open("{}/{}_stats.txt".format(save_path, name),"a")
                 f.write("{:>11}{:>12.8f}{:>12.8f}{:>30.22f}\n".format(iter_value, total_loss.item(), glparam.psnr_noisy,
                                                         np.average(glparam.net[2][0].weight.grad.data.cpu().numpy())))
-                #plt.imsave("{}/it_{}.png".format(save_path, iter_value),
-                #       np.clip(torch_to_np(glparam.out_avg), 0, 1).transpose(1, 2, 0), format="png")
+                plt.imsave("{}/it_{}.png".format(save_path, iter_value),
+                       np.clip(torch_to_np(glparam.out_avg), 0, 1).transpose(1, 2, 0), format="png")
                 
         return total_loss
         
