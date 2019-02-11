d31ab708c73c7ed6ee5330626b21c5ba577cb98e
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-11 15:06:21 +0000

    test

--- functions/generate_results.py
+++ functions/generate_results.py
@@ -21,7 +21,7 @@ def generate_result_files(path, adv, orig, num_iter, name, cuda=False):
         loaded_image = cv2.imread('{}/it_{}.png'.format(path,i*100))[..., ::-1]
         loaded_image = cv2.resize(loaded_image, (256, 256))
         img = loaded_image.copy().astype(np.float32)
-        Probs, _ = classification(img, model_name = 'resnet18', sort = False, show = False, cuda)
+        Probs, _ = classification(img, model_name = 'resnet18', sort = False, show = False, cuda=cuda)
         #Probs_np = torch_to_np(Probs)
         Probs_np= Probs
         Confidence[i,0] = Probs_np[original_class]
