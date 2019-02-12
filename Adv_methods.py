a17ec33406d808ccc7cf08297d77d22a0137f229
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-12 22:59:55 +0000

    ret

--- Adv_methods.py
+++ Adv_methods.py
@@ -11,16 +11,17 @@ image_dataset = ['snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg', 'car_wheel.jpg
 epsilon_values = [1, 5, 25, 100]
 num_iter = 5001
 
-for image in image_dataset:
-    name = '{}'.format(image.split('.')[0])
-    print("#############\n\nWorking on image: {} --- Method = FGSM".format(name))
-    for epsilon in epsilon_values:
-        adv, orig, _ = adversarial_examples("data/{}".format(image), method="FGSM", eps=epsilon, show=False, model_name='inception_v3')
-        save_path = 'results/Adv_DIP/Inception_v3/All_adv_methods/FGSM_eps{}'.format(epsilon)
-        _ = dip(adv, 'complex', 0.01, num_iter, save=True, save_path=save_path, plot=False, name=name)
-        generate_result_files(save_path, adv, orig, num_iter, name, cuda=True, model='inception_v3')
+# for image in image_dataset:
+#     name = '{}'.format(image.split('.')[0])
+#     print("#############\n\nWorking on image: {} --- Method = FGSM".format(name))
+#     for epsilon in epsilon_values:
+#         adv, orig, _ = adversarial_examples("data/{}".format(image), method="FGSM", eps=epsilon, show=False, model_name='inception_v3')
+#         save_path = 'results/Adv_DIP/Inception_v3/All_adv_methods/FGSM_eps{}'.format(epsilon)
+#         _ = dip(adv, 'complex', 0.01, num_iter, save=True, save_path=save_path, plot=False, name=name)
+#         generate_result_files(save_path, adv, orig, num_iter, name, cuda=True, model='inception_v3')
 
-for image in image_dataset:
+
+for image in ['refrigirator.jpg', 'rope.jpeg', 'knife.jpg']:
     name = '{}'.format(image.split('.')[0])
     print("#############\n\nWorking on image: {} --- Method = BI".format(name))
     for epsilon in epsilon_values:
