280c97af348df7407c4601a3962ae526373049db
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-20 15:32:14 +0000

    ret

--- dip_multiple.py
+++ dip_multiple.py
@@ -3,10 +3,12 @@ from functions.dip import *
 from functions.generate_results import *
 
 num_iter = 10001
-image_dataset = ['panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_GT.png', 'goldfish.jpg', 'whale.jpg',
-                 'dolphin.jpg', 'spider.jpg', 'labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg',
+image_dataset = ['labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg',
                  'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
                  'knife.jpg', 'rope.jpeg']
+
+# 'panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_GT.png', 'goldfish.jpg', 'whale.jpg',
+#                  'dolphin.jpg', 'spider.jpg',
 
 #for i in range(0,len(image_dataset)):
 #    image_path = image_dataset[i]
