b5aad1177ff9668e1afa956311a1f4294725a8a0
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-24 03:00:09 +0000

    ret

--- dip_multiple.py
+++ dip_multiple.py
@@ -8,22 +8,22 @@ image_dataset = ['panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_
                  'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
                  'rope.jpeg', 'knife.jpg']
         
-for i in range(13,len(image_dataset)):
+for i in range(len(image_dataset)):
     image_path = image_dataset[i]
     image_name = '{}'.format(image_path.split('.')[0])
-    save_path_common = 'results/Adv_DIP/Learning_rates/{}'
+    save_path_common = 'results/Adv_DIP/Std_investigation/{}'
 
     print("#############\n\nWorking on image: {}".format(image_name))
     adv, orig, pert = adversarial_examples("data/{}".format(image_path), method="LLCI", eps=100, show=False)
 
-    LR = [0.001, 0.1, 1]
-    for j in range(3):
+    STD = [1/128., 1/64., 4/64., 8/64., 16/64., 32/64., 64/64.]
+    for j in range(7):
         print("####\n\nTest {}".format(j+1))
 
-        save_path = save_path_common.format('Adam/lr{}'.format(j+1))
-        _ = dip(adv, 'complex', LR[j], num_iter, save=True, plot=False, save_path=save_path, name=image_name)
+        save_path = save_path_common.format('Adam/test{}'.format(j+1))
+        _ = dip(adv, 'complex', 0.01, num_iter, STD[j], save=True, plot=False, save_path=save_path, name=image_name)
         generate_result_files(save_path, adv, orig, num_iter, image_name)
 
-        # save_path = save_path_common.format('EntropySGD_std64/skip{}'.format(j))
-        # _ = dip(adv, 'skip{}'.format(j), 10, num_iter, 1/64., save=True, plot=False, save_path=save_path, OPTIMIZER="EntropySGD")
-        # generate_result_files(save_path, adv, orig, num_iter, image_name)
+        save_path = save_path_common.format('EntropySGD/test{}'.format(j))
+        _ = dip(adv, 'complex', 10, num_iter, STD[j], save=True, plot=False, save_path=save_path, name=image_name, OPTIMIZER="EntropySGD")
+        generate_result_files(save_path, adv, orig, num_iter, image_name)
