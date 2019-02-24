1e7036dabf1d837018227ab47a6c6d3115d2d9f8
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-24 11:50:26 +0000

    ret

--- dip_multiple.py
+++ dip_multiple.py
@@ -8,7 +8,7 @@ image_dataset = ['panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_
                  'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
                  'rope.jpeg', 'knife.jpg']
         
-for i in range(len(image_dataset)):
+for i in range(1,len(image_dataset)):
     image_path = image_dataset[i]
     image_name = '{}'.format(image_path.split('.')[0])
     save_path_common = 'results/Adv_DIP/Std_investigation/{}'
@@ -24,6 +24,6 @@ for i in range(len(image_dataset)):
         _ = dip(adv, 'complex', 0.01, num_iter, STD[j], save=True, plot=False, save_path=save_path, name=image_name)
         generate_result_files(save_path, adv, orig, num_iter, image_name)
 
-        save_path = save_path_common.format('EntropySGD/test{}'.format(j+1))
-        _ = dip(adv, 'complex', 10, num_iter, STD[j], save=True, plot=False, save_path=save_path, name=image_name, OPTIMIZER="EntropySGD")
-        generate_result_files(save_path, adv, orig, num_iter, image_name)
+        # save_path = save_path_common.format('EntropySGD/test{}'.format(j+1))
+        # _ = dip(adv, 'complex', 10, num_iter, STD[j], save=True, plot=False, save_path=save_path, name=image_name, OPTIMIZER="EntropySGD")
+        # generate_result_files(save_path, adv, orig, num_iter, image_name)
