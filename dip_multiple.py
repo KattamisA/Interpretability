40a1d127d870d51e5b1b605889b0152bfb7c34a2
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-25 18:51:25 +0000

    std

--- dip_multiple.py
+++ dip_multiple.py
@@ -8,7 +8,7 @@ image_dataset = ['panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_
                  'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
                  'rope.jpeg', 'knife.jpg']
         
-for i in range(10,len(image_dataset)):
+for i in range(11,len(image_dataset)):
     image_path = image_dataset[i]
     image_name = '{}'.format(image_path.split('.')[0])
     save_path_common = 'results/Adv_DIP/Std_investigation/{}'
@@ -26,21 +26,21 @@ for i in range(10,len(image_dataset)):
 
         # save_path = save_path_common.format('EntropySGD/test{}'.format(j+1))
         # _ = dip(adv, 'complex', 10, num_iter, STD[j], save=True, plot=False, save_path=save_path, name=image_name, OPTIMIZER="EntropySGD")
-        # generate_result_files(save_path, adv, orig, num_iter, image_name)
+        # generate_result_files(save_path, advc, orig, num_iter, image_name)
 
 
-for i in range(len(image_dataset)):
-    image_path = image_dataset[i]
-    image_name = '{}'.format(image_path.split('.')[0])
-    save_path_common = 'results/Adv_DIP/Std_investigation/{}'
-
-    print("#############\n\nWorking on image: {}".format(image_name))
-    adv, orig, pert = adversarial_examples("data/{}".format(image_path), method="LLCI", eps=100, show=False)
-
-    STD = [1/128., 1/64., 2/64., 8/64., 16/64., 32/64., 64/64.]
-    for j in range(7):
-        print("####\n\nTest {}".format(j+1))
-
-        save_path = save_path_common.format('EntropySGD/test{}'.format(j+1))
-        _ = dip(adv, 'complex', 10, num_iter, STD[j], save=True, plot=False, save_path=save_path, name=image_name, OPTIMIZER="EntropySGD")
-        generate_result_files(save_path, adv, orig, num_iter, image_name)
+# for i in range(len(image_dataset)):
+#     image_path = image_dataset[i]
+#     image_name = '{}'.format(image_path.split('.')[0])
+#     save_path_common = 'results/Adv_DIP/Std_investigation/{}'
+#
+#     print("#############\n\nWorking on image: {}".format(image_name))
+#     adv, orig, pert = adversarial_examples("data/{}".format(image_path), method="LLCI", eps=100, show=False)
+#
+#     STD = [1/128., 1/64., 2/64., 8/64., 16/64., 32/64., 64/64.]
+#     for j in range(7):
+#         print("####\n\nTest {}".format(j+1))
+#
+#         save_path = save_path_common.format('EntropySGD/test{}'.format(j+1))
+#         _ = dip(adv, 'complex', 10, num_iter, STD[j], save=True, plot=False, save_path=save_path, name=image_name, OPTIMIZER="EntropySGD")
+#         generate_result_files(save_path, adv, orig, num_iter, image_name)
