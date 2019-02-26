e3ebc529b479109d2d00e6ab1de6df8875122114
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-26 23:19:43 +0000

    noise rev

--- noise_adv_recovery.py
+++ noise_adv_recovery.py
@@ -9,29 +9,34 @@ image_dataset = ['panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_
                  'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
                  'rope.jpeg', 'knife.jpg']
 
-for i in range(len(image_dataset)):
-    orig_path = image_dataset[i]
-    image_name = '{}'.format(orig_path.split('.')[0])
+methods = ['LLCI_eps1', 'LLCI_eps5', 'LLCI_eps25', 'LLCI_eps100', 'BI_eps1', 'BI_eps5', 'BI_eps25', 'BI_eps100',
+           'FGSM_eps1', 'FGSM_eps5', 'FGSM_eps25', 'FGSM_eps100']
 
-    orig = cv2.imread("data/{}".format(orig_path))[..., ::-1]
-    _, ranks = classification(orig, sort=True, show=False, cuda=True)
-    original_class = ranks[0, 0]
+for meth in methods:
+    print("#######################\n\nMethod: {}".format(meth))
+    for i in range(len(image_dataset)):
+        orig_path = image_dataset[i]
+        image_name = '{}'.format(orig_path.split('.')[0])
 
-    image_path = '{}_LLCI_eps100.png'.format(image_name)
+        orig = cv2.imread("data/{}".format(orig_path))[..., ::-1]
+        _, ranks = classification(orig, sort=True, show=False, cuda=True)
+        original_class = ranks[0, 0]
 
-    adversary = cv2.imread("results/adversarial_examples/LLCI_eps100/{}".format(image_path))[..., ::-1]
-    save_path_common = 'results/Adv_rev_noise/{}'
-    save_path = save_path_common.format('LLCI_eps100')
-    f = open("{}/{}_stats.txt".format(save_path, image_name), "w+")
+        image_path = '{}_{}.png'.format(image_name, meth)
 
-    print("#############\n\nWorking on image: {}".format(image_name))
-    for std in range(1, 129):
-        print('Noise standard deviation [{:>4}/128]'.format(std), end='\r')
-        Average_confidence = 0
-        for j in range(0, 5):
-            adversary_noisy = adversary + std * np.random.randn(224, 224, 3)
-            confidence, _ = classification(orig, sort=False, show=False, cuda=True)
-            Average_confidence += confidence[0, original_class]/5
+        adversary = cv2.imread("results/adversarial_examples/{}/{}".format(meth, image_path))[..., ::-1]
+        save_path_common = 'results/Adv_rev_noise/{}'
+        save_path = save_path_common.format(meth)
+        f = open("{}/{}_stats.txt".format(save_path, image_name), "w+")
 
-        f = open("{}/{}_stats.txt".format(save_path, image_name), "a")
-        f.write("{:>8}{:>16.10f}\n".format(std, Average_confidence))
+        print("########\n\nWorking on image: {}".format(image_name))
+        for std in range(1, 129):
+            print('Noise standard deviation [{:>4}/128]'.format(std), end='\r')
+            Average_confidence = 0
+            for j in range(0, 5):
+                adversary_noisy = adversary + std * np.random.randn(224, 224, 3)
+                confidence, _ = classification(orig, sort=False, show=False, cuda=True)
+                Average_confidence += confidence[0, original_class]/5
+
+            f = open("{}/{}_stats.txt".format(save_path, image_name), "a")
+            f.write("{:>8}{:>16.10f}\n".format(std, Average_confidence))
