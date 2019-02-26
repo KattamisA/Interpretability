2ac829dbab8392eaaf3f4aab0a123ff032d819cb
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-26 16:33:53 +0000

    kernel size

--- dip_multiple.py
+++ dip_multiple.py
@@ -2,7 +2,7 @@ from functions.adversarial import *
 from functions.dip import *
 from functions.generate_results import *
 
-num_iter = 10001
+num_iter = 5001
 image_dataset = ['panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_GT.png', 'goldfish.jpg', 'whale.jpg',
                  'dolphin.jpg', 'spider.jpg', 'labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg',
                  'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
