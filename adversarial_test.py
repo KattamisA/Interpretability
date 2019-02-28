389973ce734e0e6582308f5e5775e08e43fd703e
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-28 14:48:43 +0000

    std

--- adversarial_test.py
+++ adversarial_test.py
@@ -14,7 +14,7 @@ plt.imsave("data/{}_JSMA.png".format(image_path.split('.')[0]), adv, format='png
 adv = adv.astype(np.float32)
 orig = orig.astype(np.float32)
 diff = np.absolute(adv-orig)
-plt.imsave("data/{}_JSMA_pert.png".format(image_path), diff.astype(np.int8), format='png')
-print(diff)
+plt.imsave("data/{}_JSMA_pert.png".format(image_path), diff.astype(np.int8)*20, format='png')
+print(np.count_nonzero(diff))
 
 
