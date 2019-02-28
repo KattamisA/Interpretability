eb98e6ed628c32e3490ec54036437b4e34a514d1
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-28 14:29:24 +0000

    std

--- adversarial_test.py
+++ adversarial_test.py
@@ -6,14 +6,11 @@ image_path = 'panda.jpg'
 
 # adv, _, pert = adversarial_examples("data/{}".format(image_path), method="JSMA", eps=1, show=True, cuda=True)
 
-adv, _, pert = adversarial_examples("data/{}".format(image_path), method="JSMA", eps=100, show=True, num_iter=125, cuda=True)
+adv, orig, pert = adversarial_examples("data/{}".format(image_path), method="JSMA", eps=100, show=True, num_iter=125, cuda=True)
 
-plt.imsave("data/{}_JSMA_std15.png".format(image_path.split('.')[0]), adv, format='png')
+plt.imsave("data/{}_JSMA.png".format(image_path.split('.')[0]), adv, format='png')
 
-# adv = cv2.imread("data/{}_JSMA.png".format(image_path))[..., ::-1]
-# orig = cv2.imread("data/{}".format(image_path))[..., ::-1]
-# orig = cv2.resize(orig, (224, 224))
-# plt.imsave("data/{}_JSMA_pert.png".format(image_path), (adv-orig)*50.0, format='png')
-# print(adv-orig)
+plt.imsave("data/{}_JSMA_pert.png".format(image_path), (adv-orig)*50.0, format='png')
+print(adv-orig)
 
 
