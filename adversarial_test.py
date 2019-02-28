bda00cc2a18a3f8ae1b0dbb35c01b7b74710e18a
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-28 16:27:51 +0000

    ret

--- adversarial_test.py
+++ adversarial_test.py
@@ -9,7 +9,7 @@ image_dataset = ['panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_
                  'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
                  'rope.jpeg', 'knife.jpg']
 
-epsilons = [2*i + 1 for i in range(56)]
+epsilons = [2*i + 1 for i in range(51)]
 
 for i in range(1, len(image_dataset)):
     image_path = image_dataset[i]
