8dc79ac44afd0c5e021ea964fd052355d50cb91a
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-28 12:49:30 +0000

    std

--- adversarial_test.py
+++ adversarial_test.py
@@ -7,8 +7,8 @@ image_path = 'panda.jpg'
 adv, _, pert = adversarial_examples("data/{}".format(image_path), method="FGSM", eps=100, show=True, cuda=False)
 print(pert)
 
-adv, _, pert = adversarial_examples("data/{}".format(image_path), method="JSMA", eps=100, show=True, num_iter=15, cuda=True)
-# print(pert)
+adv, _, pert = adversarial_examples("data/{}".format(image_path), method="JSMA", eps=100, show=True, num_iter=125, cuda=True)
+print(pert)
 
 plt.imsave("data/{}_JSMA.png".format(image_path.split('.')[0]), adv, format='png')
 
