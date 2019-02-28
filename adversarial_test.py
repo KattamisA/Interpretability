ecfc04f2d2a1a04d839d866a3e94419287c6e9d7
Andreas Kattamis <kattamis.a@gmail.com>
2019-02-28 12:32:07 +0000

    std

--- adversarial_test.py
+++ adversarial_test.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 
 image_path = 'panda.jpg'
 
-adv, _, pert = adversarial_examples("data/{}".format(image_path), method="FGSM", eps=100, show=True)
+adv, _, pert = adversarial_examples("data/{}".format(image_path), method="JSMA", eps=1, show=True)
+print(pert)
+
+adv, _, pert = adversarial_examples("data/{}".format(image_path), method="JSMA", eps=100, show=True)
 # print(pert)
 
 plt.imsave("data/{}_JSMA.png".format(image_path.split('.')[0]), adv, format='png')
