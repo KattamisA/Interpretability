13bfca1e226eedfebafa425cfa08571815c0cb3d
Andreas Kattamis <kattamis.a@gmail.com>
2019-03-01 23:42:30 +0000

    ret

--- denoising.py
+++ denoising.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 ### Observing multiple images
-num_iter = 5001
+num_iter = 101
 image_dataset = ['panda.jpg', 'peacock.jpg', 'F16_GT.png', 'monkey.jpg', 'zebra_GT.png', 'goldfish.jpg', 'whale.jpg',
                  'dolphin.jpg', 'spider.jpg', 'labrador.jpg', 'snake.jpg', 'flamingo_animal.JPG', 'canoe.jpg',
                  'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
@@ -14,7 +14,7 @@ for i in range(len(image_dataset)):
     image_name = '{}'.format(image_path.split('.')[0])
     orig = cv2.imread('data/{}'.format(image_path))[..., ::-1]
     img_noisy = cv2.imread('results/Denoising/dataset/{}_noisy.png'.format(image_name))
-
+    print(np.shape(img_noisy))
     save_path = 'results/Denoising/Baseline'
     _ = dip(img_noisy, 'complex', 0.01, num_iter, save=True, plot=False, save_path=save_path, name=image_name)
     generate_result_files(save_path, img_noisy, orig, num_iter, image_name)
