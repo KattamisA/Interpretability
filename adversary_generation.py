8fceb88dc369f5bb17833812a9a546ed9cd6193a
Andreas Kattamis <kattamis.a@gmail.com>
2019-03-01 22:37:58 +0000

    ret

--- adversary_generation.py
+++ adversary_generation.py
@@ -15,14 +15,14 @@ for i in range(len(image_dataset)):
 
     print("#############\n\nWorking on image: {}".format(image_name))
 
-    for eps in [1, 5, 25, 100]:
-        adv, _, _ = adversarial_examples("data/{}".format(image_path), method="LLCI", eps=eps, show=False)
-        plt.imsave(save_path_common.format('LLCI_eps{}/{}_LLCI_eps{}.png'.format(eps, image_name, eps)), adv, format='png')
+    for eps in [1, 5, 25]:
+        adv, _, _ = adversarial_examples("data/{}".format(image_path), method="JSMA", eps=eps, show=False)
+        plt.imsave(save_path_common.format('JSMA_eps{}/{}_JSMA_eps{}.png'.format(eps, image_name, eps)), adv, format='png')
 
-        adv, _, _ = adversarial_examples("data/{}".format(image_path), method="BI", eps=eps, show=False)
-        plt.imsave(save_path_common.format('BI_eps{}/{}_BI_eps{}.png'.format(eps, image_name, eps)), adv, format='png')
-
-        adv, _, _ = adversarial_examples("data/{}".format(image_path), method="FGSM", eps=eps, show=False)
-        plt.imsave(save_path_common.format('FGSM_eps{}/{}_FGSM_eps{}.png'.format(eps, image_name, eps)), adv, format='png')
+        # adv, _, _ = adversarial_examples("data/{}".format(image_path), method="BI", eps=eps, show=False)
+        # plt.imsave(save_path_common.format('BI_eps{}/{}_BI_eps{}.png'.format(eps, image_name, eps)), adv, format='png')
+        #
+        # adv, _, _ = adversarial_examples("data/{}".format(image_path), method="FGSM", eps=eps, show=False)
+        # plt.imsave(save_path_common.format('FGSM_eps{}/{}_FGSM_eps{}.png'.format(eps, image_name, eps)), adv, format='png')
 
 
