bd3c8d10e1a98ed0d62b4679e261afd4a5979276
Andreas Kattamis <kattamis.a@gmail.com>
2019-03-01 15:34:09 +0000

    depth and entropy tests

--- denoising.py
+++ denoising.py
@@ -16,22 +16,20 @@ for i in range(len(image_dataset)):
     orig = cv2.imread('data/{}'.format(image_path))
     img_noisy = cv2.imread('results/Denoising/dataset/{}_noisy.png'.format(image_name))
     
-    save_path = 'results/Denoising/Baseline'
-    _ = dip(img_noisy, 'complex', 0.01, num_iter, save=True, plot=False, save_path=save_path, name=image_name)
+    save_path = 'results/Denoising/EntropySGD'
+    _ = dip(img_noisy, 'complex', 1, num_iter, save=True, plot=False, save_path=save_path, name=image_name, OPTIMIZER="EntropySGD")
     generate_result_files(save_path, img_noisy, orig, num_iter, image_name)
-
-
 
 for i in range(len(image_dataset)):
     image_path = image_dataset[i]
     image_name = '{}'.format(image_path.split('.')[0])
     orig = cv2.imread('data/{}'.format(image_path))
     img_noisy = cv2.imread('results/Denoising/dataset/{}_noisy.png'.format(image_name))
-    save_path_common = 'results/Denoising/Architecture/{}'
+    save_path_common = 'results/Denoising/depth_tests/{}'
 
-    for j in range(1, 5):
-        save_path = save_path_common.format('Arch{}'.format(j))
-        _ = dip(img_noisy, 'test{}'.format(j), 0.01, num_iter, save=True, plot=False, save_path=save_path, name=image_name)
+    for j in range(1, 6):
+        save_path = save_path_common.format('depth{}'.format(j))
+        _ = dip(img_noisy, 'depth{}'.format(j), 0.01, num_iter, save=True, plot=False, save_path=save_path, name=image_name)
         generate_result_files(save_path, img_noisy, orig, num_iter, image_name)
 
 
