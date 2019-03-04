0219816cf6d398dc6ee13aff2e57de3d9e393293
Andreas Kattamis <kattamis.a@gmail.com>
2019-03-04 01:09:21 +0000

    ret

--- saliency_tests.py
+++ saliency_tests.py
@@ -8,11 +8,25 @@ import matplotlib.pyplot as plt
 #                  'car_wheel.jpg', 'fountain.jpg', 'football_helmet.jpg', 'hourglass.jpg', 'refrigirator.jpg',
 #                  'knife.jpg', 'rope.jpeg']
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 image_dataset2 = ['it_{}.png'.format(100*i) for i in range(0, 11)]
 image_dataset2.extend(['it_{}.png'.format(200*i) for i in range(6, 51)])
 
-# f = open('results/Saliency/adversarial/llci/LLCI_classification.txt', "w+")
-#q = open('results/Saliency/adversarial/llci/LLCI_classification_inception_v3.txt', "w+")
 
 for i in range(len(image_dataset2)):
     image = image_dataset2[i]
@@ -34,16 +48,3 @@ for i in range(len(image_dataset2)):
 
     generate_saliency_maps(save_path, img_path, name, model_type='resnet18', cuda=True, target_label=target_class,
                         top_percentile=95, bottom_percentile=10, mask_mode=True, stdev_spread=0.01, dual=False)
-
-
-# for i in range(len(image_dataset2)):
-#      image = image_dataset2[i]
-#
-#      print('###### Working on image: ' + image.split('.')[0])
-#      generate_saliency_maps('results/Adv_DIP/Multiple_images/knife', image, model_type='resnet18', cuda=True,
-#                             top_percentile=99, bottom_percentile=10, mask_mode=True)
-#
-#      #generate_saliency_maps('results/Adv_DIP/Multiple_images/knife', "adv_knife.png", model_type='resnet18', cuda=True,
-#      #                      top_percentile=99, bottom_percentile=0, mask_mode=True, target_label_index=class_index)
-#      print('\n')
-
