953c2fb52c72cb53596713fa5d2e6af6b000a59b
Andreas Kattamis <kattamis.a@gmail.com>
2019-05-16 20:16:27 +0100

    defence

--- Defence.py
+++ Defence.py
@@ -21,17 +21,16 @@ for i in range(len(image_dataset)):
     print(image_name)
     orig = cv2.imread("data/" + image_path)[..., ::-1]
     _, ranks = classification(orig, sort=True, show=False, model_name='resnet18', cuda=True)
-    orig_rank = ranks[0,0]
+    orig_rank = ranks[0, 0]
     adv = cv2.imread("results/adversarial_examples/Examples/FGSM_eps1/" + image_name + "_FGSM_eps1.png")[..., ::-1]
     _, ranks = classification(adv , sort=True, show=False, model_name='resnet18', cuda=True)
-    if ranks[0,0] == orig_rank:
+    if ranks[0, 0] == orig_rank:
         p = p + 1
     output = dip(adv, 'complex', 0.01, 301, save=False, plot=False, name=image_name)
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -52,8 +51,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -74,8 +72,6 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -96,8 +92,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -118,8 +113,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -140,8 +134,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -162,8 +155,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -184,8 +176,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -206,8 +197,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -228,8 +218,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -250,8 +239,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -272,8 +260,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -294,8 +281,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -316,8 +302,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -338,8 +323,7 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
 
@@ -360,7 +344,6 @@ for i in range(len(image_dataset)):
     _, ranks = classification(output * 255, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q = q + 1
-    print(q)
-print("\n\n#Results: Adversary - " + p/20.0 + "   Recovered - "  + q/20.0)
+print("\n\nResults: Adversary - {}      Recovered - {}".format(p/20.0,q/20.0))
 z.append([q / 20.0, p / 20.0])
 print(z)
