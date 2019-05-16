8dc9f8a00c8b7c0a36f34c350df6fcb642c6ccc5
Andreas Kattamis <kattamis.a@gmail.com>
2019-05-16 17:04:55 +0100

    defence

--- Defence.py
+++ Defence.py
@@ -20,7 +20,8 @@ for i in range(len(image_dataset)):
     orig_rank = ranks[0,0]
     adv = cv2.imread("results/adversarial_examples/Examples/FGSM_eps100/" + image_name + "_FGSM_eps100.png")[..., ::-1]
     output = dip(adv, 'complex', 0.01, 100, save=False, plot=False, name=image_name)
-    print(np.shape(output))
+    # print(np.shape(output))
+    ouput = output.transpose(1, 2, 0)
     _, ranks = classification(output, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks[0,0] == orig_rank:
         q += 1
