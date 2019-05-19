06f05dce246aaf7706a8bfbadf1abb82a52595d1
Andreas Kattamis <kattamis.a@gmail.com>
2019-05-19 22:58:36 +0100

    ret

--- correctly_classified_images.py
+++ correctly_classified_images.py
@@ -9,7 +9,7 @@ contents = contents.split()
 wnid = [contents[i] for i in range(0,400,2)]
 classid = [contents[i] for i in range(1,400,2)]
 
-f = open("Class_labels.txt", "w+")
+f = open("Class_labels2.txt", "w+")
 counter = 0
 for i in range(1, 200):
     for j in range(10):
@@ -18,9 +18,9 @@ for i in range(1, 200):
             image = cv2.resize(image, (299, 299))
             _, ranks = classification(image, sort=True, show=False, model_name='InceptionV3', cuda=False)
             if ranks[0, 0] == int(classid[i]):
-                plt.imsave('correctly_classified_dataset/Image_{}.png'.format(counter), image, format ='png')
+                plt.imsave('correctly_classified_dataset2/Image_{}.png'.format(counter), image, format ='png')
                 counter = counter + 1
-                f = open("Class_labels.txt", "a")
+                f = open("Class_labels2.txt", "a")
                 f.write("{}\n".format(classid[i]))
             print(counter)
         except:
