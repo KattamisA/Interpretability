dfb9977b27108297b82b1daa0f14092fed89b109
Andreas Kattamis <kattamis.a@gmail.com>
2019-05-21 15:16:01 +0100

    run

---
+++ classifying_defence_results.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+from functions.adversarial import *
+from functions.dip import *
+import numpy as np
+import cv2
+
+f = open("data/ImageNet_Dataset/Class_labels.txt", 'r')
+classids = f.read()
+classids= classids.split()
+
+defence = np.zeros((4, 1))
+num = 0
+print('\nClean')
+for i in range(0, 15, 3):
+    num = num + 1
+    for j in range(250,1001,250):
+        image = cv2.imread("results/Defence/Clean/Image_{}_{}it.png".format(i, j))[..., ::-1]
+        _, ranks_rec = classification(image, sort=True, show=False, model_name='resnet18', cuda=False)
+        if ranks_rec[0, 0] == int(classids[i]):
+            defence[int(j/250)-1] = defence[int(j/250)-1] + 1
+    print("Clean {:>3} --- 250it: {:>7.3f}    500it: {:>7.3f}    750it: {:>7.3f}    100it: {:>7.3f}"
+          .format(num, float(defence[0])/num, float(defence[1])/num, float(defence[2])/num, float(defence[3])/num), end='\r')
+
+w = open("results/Defence/Results_clean.txt",'w+')
+w.write("Clean: {:>10.5f} {:>10.5f} {:>10.5f} {:>10.5f}\n".format(float(defence[0])/num, float(defence[1])/num, float(defence[2])/num, float(defence[3])/num))
--- defence_datasets_generation.py
+++ defence_datasets_generation.py
@@ -9,18 +9,18 @@ classids = f.read()
 classids= classids.split()
 
 
-correct = 0
-num = 0
-print('\nClean')
-for i in range(0, len(classids), 3):
-    num = num + 1
-    orig = cv2.imread("data/ImageNet_Dataset/correctly_classified_dataset/Image_{}.png".format(i))[..., ::-1]
-    orig = cv2.resize(orig, (224,224))
-    output = dip(orig, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/Clean', name = "Image_{}".format(i))
-    _, ranks_rec = classification(output*255.0, sort=True, show=False, model_name='resnet18', cuda=True)
-    if ranks_rec[0,0] == int(classids[i]):
-        correct = correct + 1
-    print("Results after {:>3}: DIP: {:>7.3f}".format(num, float(correct)/num), end='\r')
+# correct = 0
+# num = 0
+# print('\nClean')
+# for i in range(0, len(classids), 3):
+#     num = num + 1
+#     orig = cv2.imread("data/ImageNet_Dataset/correctly_classified_dataset/Image_{}.png".format(i))[..., ::-1]
+#     orig = cv2.resize(orig, (224,224))
+#     output = dip(orig, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/Clean', name = "Image_{}".format(i))
+#     _, ranks_rec = classification(output*255.0, sort=True, show=False, model_name='resnet18', cuda=True)
+#     if ranks_rec[0,0] == int(classids[i]):
+#         correct = correct + 1
+#     print("Results after {:>3}: DIP: {:>7.3f}".format(num, float(correct)/num), end='\r')
 
 # w = open("results/Defence/Results_clean.txt",'w+')
 # w.write("Clean: {:>10.5f} {:>10.5f}\n".format(float(correct)/num, float(correct)/num))
@@ -32,7 +32,7 @@ print('\nFGSM - 2')
 for i in range(0, len(classids), 3):
     num = num + 1
     adv, _, _ = adversarial_examples("data/ImageNet_Dataset/correctly_classified_dataset/Image_{}.png".format(i), eps=2, show=False)
-    output = dip(orig, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/FGSM2', name = "Image_{}".format(i))
+    output = dip(adv, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/FGSM2', name = "Image_{}".format(i))
     _, ranks_rec = classification(output*255.0, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks_rec[0,0] == int(classids[i]):
         defence_rec = defence_rec + 1
@@ -49,7 +49,7 @@ print('\nFGSM - 5')
 for i in range(0, len(classids), 3):
     num = num + 1
     adv, _, _ = adversarial_examples("data/ImageNet_Dataset/correctly_classified_dataset/Image_{}.png".format(i), eps=5, show=False)
-    output = dip(orig, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/FGSM5', name = "Image_{}".format(i))
+    output = dip(adv, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/FGSM5', name = "Image_{}".format(i))
     _, ranks_rec = classification(output*255.0, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks_rec[0,0] == int(classids[i]):
         defence_rec = defence_rec + 1
@@ -66,7 +66,7 @@ print('\nFGSM - 10')
 for i in range(0, len(classids), 3):
     num = num + 1
     adv, _, _ = adversarial_examples("data/ImageNet_Dataset/correctly_classified_dataset/Image_{}.png".format(i), eps=10, show=False)
-    output = dip(orig, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/FGSM10', name = "Image_{}".format(i))
+    output = dip(adv, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/FGSM10', name = "Image_{}".format(i))
     _, ranks_rec = classification(output*255.0, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks_rec[0,0] == int(classids[i]):
         defence_rec = defence_rec + 1
@@ -84,7 +84,7 @@ print('\nBI - 10')
 for i in range(0, len(classids), 3):
     num = num + 1
     adv, _, _ = adversarial_examples("data/ImageNet_Dataset/correctly_classified_dataset/Image_{}.png".format(i), eps=10, show=False, method='BI')
-    output = dip(orig, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/BI10', name = "Image_{}".format(i))
+    output = dip(adv, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/BI10', name = "Image_{}".format(i))
     _, ranks_rec = classification(output*255.0, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks_rec[0,0] == int(classids[i]):
         defence_rec = defence_rec + 1
@@ -101,7 +101,7 @@ print('\nLLCI - 10')
 for i in range(0, len(classids), 3):
     num = num + 1
     adv, _, _ = adversarial_examples("data/ImageNet_Dataset/correctly_classified_dataset/Image_{}.png".format(i), eps=10, show=False, method='LLCI')
-    output = dip(orig, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/LLCI10', name = "Image_{}".format(i))
+    output = dip(adv, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/LLCI10', name = "Image_{}".format(i))
     _, ranks_rec = classification(output*255.0, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks_rec[0,0] == int(classids[i]):
         defence_rec = defence_rec + 1
@@ -117,7 +117,7 @@ print('\nJSMA - 10')
 for i in range(0, len(classids), 3):
     num = num + 1
     adv, _, _ = adversarial_examples("data/ImageNet_Dataset/correctly_classified_dataset/Image_{}.png".format(i), eps=10, show=False, method='JSMA', cuda=True)
-    output = dip(orig, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/JSMA10', name = "Image_{}".format(i))
+    output = dip(adv, 'complex', 0.01, 1001, plot=False, save=True, save_path = 'results/Defence/JSMA10', name = "Image_{}".format(i))
     _, ranks_rec = classification(output*255.0, sort=True, show=False, model_name='resnet18', cuda=True)
     if ranks_rec[0,0] == int(classids[i]):
         defence_rec = defence_rec + 1
