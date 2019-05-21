1fb9a1df138ea7e22ab3238ef534cd5a82416c16
Andreas Kattamis <kattamis.a@gmail.com>
2019-05-21 09:02:21 +0100

    ret

--- defence_datasets_generation.py
+++ defence_datasets_generation.py
@@ -8,26 +8,6 @@ f = open("data/ImageNet_Dataset/Class_labels.txt", 'r')
 classids = f.read()
 classids= classids.split()
 
-no_defence_rec = 0
-defence_rec = 0
-num = 0
-print('\nJSMA - 10')
-for i in range(0, len(classids), 3):
-    num = num + 1
-    adv, _, _ = adversarial_examples("data/ImageNet_Dataset/correctly_classified_dataset/Image_{}.png".format(i), eps=10, show=False, method='JSMA', cuda=True)
-    _, ranks_adv = classification(adv, sort=True, show=False, model_name='resnet18', cuda=True)
-    # plt.imsave("data/adversarial_defence_datasets/FGSM2/adv_Image_{}.png".format(i), adv, format='png')
-    output = dip(adv, 'complex', 0.01, 501, save=False, plot=False)
-    _, ranks_rec = classification(output*255.0, sort=True, show=False, model_name='resnet18', cuda=True)
-    if ranks_adv[0,0] == int(classids[i]):
-        no_defence_rec = no_defence_rec + 1
-    if ranks_rec[0,0] == int(classids[i]):
-        defence_rec = defence_rec + 1
-    print("Results after {:>3}: Defence: {:>7.3f} --- No defence: {:>7.3f}".format(num, float(defence_rec)/num, float(no_defence_rec)/num), end='\r')
-
-w = open("results/Defence/Results_JSMA10.txt",'w+')
-w.write("JSMA10: {:>10.5f} {:>10.5f}\n".format(float(defence_rec)/num, float(no_defence_rec)/num))
-
 
 correct = 0
 num = 0
--- results/Denoising/depth_tests/Plotting_depth.m
+++ results/Denoising/depth_tests/Plotting_depth.m
@@ -5,10 +5,10 @@ img_names = ["panda", "peacock", "F16_GT", "monkey",'zebra_GT','goldfish','whale
 
 common = 'depth%d/%s_PSNR.txt';
 Value = zeros(51,20);
-Average = zeros(51,6);
-standard_deviation = zeros(51,6);
+Average = zeros(51,7);
+standard_deviation = zeros(51,7);
 
-for j=1:6
+for j=1:7
     for i=1:size(img_names,2)
         path = sprintf(common,j,img_names(i));
         s = load(path);
@@ -19,11 +19,11 @@ for j=1:6
 end
 
 figure
-plot(0:100:5000, Average, 'linewidth', 1.3)
+plot(0:100:5000, Average(:, 1:6), 'linewidth', 1.3)
 xlabel('DIP iterations')
 ylabel('PSNR')
 grid on
-legend('Depth = 1','Depth = 2','Depth = 3', 'Depth = 4','Depth = 5 (Baseline)','Depth = 6')
+legend('1-layer encoder','2-layer encoder','3-layer encoder','4-layer encoder','5-layer encoder - Baseline','6-layer encoder')%,'7-layer encoder')
 ylim([10 32])
 
 % figure
