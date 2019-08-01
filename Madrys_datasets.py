fa65b3f378206d82f9336b5986d76b5aeb5abcf6
Andreas Kattamis <kattamis.a@gmail.com>
2019-08-01 22:17:43 +0300

    ret

--- Madrys_datasets.py
+++ Madrys_datasets.py
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 data_path = "data/non_robust_CIFAR"
 
 train_labels = ch.cat(ch.load(os.path.join(data_path, "CIFAR_lab")))
+print(train_labels)
 train_labels = train_labels.numpy()
+print(train_labels)
 num_iter = 5001
 for i in range(10):
     print("############# Working on image: {}/500".format(i+1))
