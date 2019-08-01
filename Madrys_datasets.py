44c0cf1147103fcbe5be081f23eb38c044c028ca
Andreas Kattamis <kattamis.a@gmail.com>
2019-08-01 15:13:06 +0300

    ret

--- Madrys_datasets.py
+++ Madrys_datasets.py
@@ -11,6 +11,7 @@ data_path = "data/robust_CIFAR"
 train_labels = ch.cat(ch.load(os.path.join(data_path, "CIFAR_lab")))
 num_iter = 5000
 for i in range(10):
+    print("############# Working on image: {}/500".format(i+1))
     image = cv2.imread(data_path + '/' + str(i) + '.png')[..., ::-1]
     save_path = 'results/Features/robust'
     output = dip(image, 'depth3', num_iter=num_iter, save=True, save_path=save_path, name=str(i))
