52499ca5e50684c0d1802fff8933ad0d787efd05
Andreas Kattamis <kattamis.a@gmail.com>
2019-08-01 22:38:49 +0300

    ret

--- functions/dip.py
+++ functions/dip.py
@@ -353,8 +353,8 @@ def dip(img_np, arch='default', LR=0.01, num_iter=1000, reg_noise_std=1.0/30, ex
     if loss_fn == 'KLDiv':
         criterion = torch.nn.KLDivLoss().type(dtype)
         
-    # if save == True:
-        # f = open("{}/{}_stats.txt".format(save_path, name),"w+")
+    if save == True:
+        f = open("{}/{}_stats.txt".format(save_path, name),"w+")
         # f.write("{:>11}{:>12}{:>12}\n".format('Iterations','Total_Loss','PSNR'))
         # save_net_details(save_path, arch, param_numbers, pad, OPT_OVER, OPTIMIZER, input_depth,
         #          loss_fn = loss_fn, LR = LR, num_iter = num_iter, exp_weight = glparam.exp,
--- results/Features/features_plotting.m
+++ results/Features/features_plotting.m
@@ -1,22 +1,24 @@
 clear all
 close all
 
-values = zeros(10,51);
+values = zeros(10,11);
 for i = 1:10
-    name = sprintf('robust/%d_stats.txt',i-1);
+    name = sprintf('non_robust/%d_Confidences.txt',i-1);
     q = load(name);
-    values(i,:) = q(:,2);
+    values(i,:) = q(:,1);
 end
 
 averaged(1,:) = mean(values);
+% 
+% values = zeros(10,51);
+% for i = 1:10
+%     name = sprintf('non_robust/%d_Normalised.txt',i-1);
+%     q = load(name);
+%     values(i,:) = q(:,1);
+% end
+% 
+% averaged(2,:) = mean(values);
+% 
+% plot(0:100:5000,averaged)
 
-values = zeros(10,51);
-for i = 1:10
-    name = sprintf('non_robust/%d_stats.txt',i-1);
-    q = load(name);
-    values(i,:) = q(:,2);
-end
-
-averaged(2,:) = mean(values);
-
-plot(0:100:5000,averaged)
+plot(0:100:1000, averaged)
