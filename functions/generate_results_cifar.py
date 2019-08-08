ab6d498018a2f993743d7db5b8ab20e528f8cacf
Andreas Kattamis <kattamis.a@gmail.com>
2019-08-08 12:56:21 +0300

    ret

--- functions/generate_results_cifar.py
+++ functions/generate_results_cifar.py
@@ -13,12 +13,12 @@ def generate_result_files_cifar(path, orig, num_iter, name):
     model = WideResNet(depth=34, num_classes=10, widen_factor=10, dropRate=0.0)
 
     load_model(model, "cifar_model/cifar-10_default/checkpoint_12000.pth")
-    num_images = int((num_iter-1)/100 + 1)
+    num_images = int((num_iter-1)/10 + 1)
 
     Confidence = np.ones([num_images, 10])
     orig = orig.astype(np.float32)
     for i in range(num_images):
-        loaded_image = cv2.imread('{}/it_{}.png'.format(path,i *100))[..., ::-1]
+        loaded_image = cv2.imread('{}/it_{}.png'.format(path,i *10))[..., ::-1]
         img = loaded_image.copy().astype(np.float32)
         img = np.transpose(img, [2,0,1])
         img = np_to_torch(img)
--- results/Features/features_plotting.m
+++ results/Features/features_plotting.m
@@ -1,7 +1,7 @@
 clear all
 close all
 
-values = zeros(10,11);
+values = zeros(10,101);
 labels = load('robust/labels.txt');
 for i = 1:10
     name = sprintf('robust/%d_Confidences.txt',i-1);
@@ -11,7 +11,7 @@ end
 
 averaged(1,:) = mean(values,1);
 
-plot(0:100:1000, averaged)
+plot(0:10:1000, averaged)
 ylim([0 0.5])
 
 values = zeros(10,11);
@@ -25,5 +25,5 @@ end
 averaged(1,:) = mean(values);
 
 hold on
-plot(0:100:1000, averaged)
+plot(0:10:1000, averaged)
 ylim([0 0.2])
