3ebef5244683341a422d7cd34c444b8416b3e534
Andreas Kattamis <kattamis.a@gmail.com>
2019-08-08 12:28:34 +0300

    ret

--- results/Features/features_plotting.m
+++ results/Features/features_plotting.m
@@ -9,6 +9,21 @@ for i = 1:10
     values(i,:) = q(:,labels(i)+1);
 end
 
-averaged(1,:) = mean(values);
+averaged(1,:) = mean(values,1);
 
 plot(0:100:1000, averaged)
+ylim([0 0.5])
+
+values = zeros(10,11);
+labels = load('non_robust/labels.txt');
+for i = 1:10
+    name = sprintf('robust/%d_Confidences.txt',i-1);
+    q = load(name);
+    values(i,:) = q(:,labels(i)+1);
+end
+
+averaged(1,:) = mean(values);
+
+hold on
+plot(0:100:1000, averaged)
+ylim([0 0.2])
